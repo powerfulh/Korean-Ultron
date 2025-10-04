4fdd21c94f620339c6913af6320575dff9c7da00
powerfulh <68536042+powerfulh@users.noreply.github.com>
2025-10-04 21:34:56 +0900

    Adjust

--- src/main/java/root/Core.java
+++ src/main/java/root/Core.java
@@ -71,7 +71,7 @@ public class Core {
     public List<Map<String, Object>> v1(@Valid @Size(min = 1, max = 18) String pureSrc, boolean export) {
         Map<Integer, List<List<UltronContext>>> listMap = new HashMap<>();
         var targetListFull = mapper.selectGenerationTarget(understand(pureSrc).stream().map(Toke::getN).toList());
-        var targetList = targetListFull.subList(0, Math.min(targetListFull.size(), pureSrc.length() * 2)); // 말이 너무 주절주절 되는 거 같아서..
+        var targetList = targetListFull.subList(0, Math.min(targetListFull.size(), pureSrc.length() * 3)); // 말이 너무 주절주절 되는 거 같아서..
         Map<Integer, List<UltronHistory>> historyList = new HashMap<>();
         targetList.forEach(item -> {
             var list = listMap.get(item.getLeftword());
